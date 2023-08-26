ccc82d0618c49b0ada0c037155d4b4732eced996
Steve Bassette <steve.bassette@gmail.com>
2023-08-26 12:47:34 -0400

    Refactors POC demo code into object

--- connect4.py
+++ connect4.py
@@ -32,101 +32,109 @@ class Game():
                 return True
         return False
 
-class UI():
-    def __init__(self):
-        stdscr = curses.initscr()
+class CliGame(Game):
+    def __init__(self, stdscr):
+        super().__init__(6,6)
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(True)
+        self.stdscr = stdscr
+
+    def drop(self, token, column):
+        super().drop(token, column)
+
+    def main(self):
+        stdscr = self.stdscr
+        state = "new"
+
+        # game = self
+        red = self.token('r')
+        black = self.token('b')
+
+        players_list = [red, black]
+
+        selected_column = 0
+        turn = 0
+        while True:
+            if state == "new":
+                stdscr.addstr(0, 0, 'Press "t" for test' )
+                stdscr.addstr(1, 0, 'Press "d" for divide' )
+                stdscr.addstr(2, 0, 'Press "g" for game' )
+
+            c = stdscr.getch()
+
+
+            if c == ord('t'):
+                stdscr.clear()
+                state = "testing"
+                stdscr.addstr("test")
+
+            if c == ord('d'):
+                state = "dividing"
+                stdscr.clear()
+                stdscr.addstr(1, 0, '10 divided by {} is {}'.format(1, 10/1))
+                stdscr.addstr(7, 0, '10 divided by {} is {}'.format(1, 10/1))
+
+            if c == ord('g'):
+                state = "gaming"
+
+            if state == "gameover" and c == ord('r'):
+                state = "gaming"
+                self.grid = dict()
+
+            if c == curses.KEY_RIGHT:
+                selected_column += 1
+                selected_column = min(selected_column, self.width-1)
+
+            if c == curses.KEY_LEFT:
+                selected_column -= 1
+                selected_column = max(selected_column, 0)
+
+            if c == curses.KEY_DOWN and state == "gaming":
+                self.drop(players_list[turn%len(players_list)], selected_column)
+                turn += 1
+
+            if state == "gaming":
+                stdscr.clear()
+                stdscr.addstr(0, selected_column, 'v')
+                stdscr.addstr(1, 0, '_'*self.width)
+                stdscr.addstr(8, 0, '~'*self.width)
+
+                for k in self.grid:
+                    stdscr.addstr(self.height-k[0]+1, k[1], self.grid[k])
+
+            if self.over():
+                state = "gameover"
+                stdscr.addstr(10, 0, 'GAME OVER ....')
+                stdscr.addstr(11, 0, 'Press:')
+                stdscr.addstr(12, 0, '       r) restart')
+                stdscr.addstr(13, 0, '       q) quit')
+                # stdscr.addstr(10, 0, ' _________    _______    _      _    _____   ')
+                # stdscr.addstr(11, 0, '|  _______|  |  ___  |  | \    / |  |  ___|  ')
+                # stdscr.addstr(12, 0, '| |    ___   | |___| |  |  \  /  |  | |___   ')
+                # stdscr.addstr(13, 0, '| |   |_  |  |  ___  |  |   \/   |  |  ___|  ')
+                # stdscr.addstr(14, 0, '| |_____| |  | |   | |  | |\  /| |  | |___   ')
+                # stdscr.addstr(15, 0, '|_________|  |_|   |_|  |_| \/ |_|  |_____|  ')
+
+                # stdscr.addstr(17, 0, ' ________   __        __   _____    _____    ')
+                # stdscr.addstr(18, 0, '|  ____  |  \ \      / /  |  ___|  |  _  |   ')
+                # stdscr.addstr(19, 0, '| |    | |   \ \    / /   | |___   | |_| |   ')
+                # stdscr.addstr(20, 0, '| |    | |    \ \  / /    |  ___|  |    _|   ')
+                # stdscr.addstr(21, 0, '| |____| |     \ \/ /     | |___   | |\ \    ')
+                # stdscr.addstr(22, 0, '|________|      \__/      |_____|  |_| \_\   ')
+                stdscr.refresh()
+
+            if c == ord('q'):
+                break
+            
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
 
 def main_ui(stdscr):
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(True)
-    state = "new"
-
-    game = Game(6, 6)
-    red = game.token('r')
-    black = game.token('b')
-
-    players_list = [red, black]
-
-    selected_column = 0
-    turn = 0
-    while True:
-        if state == "new":
-            stdscr.addstr(0, 0, 'Press "t" for test' )
-            stdscr.addstr(1, 0, 'Press "d" for divide' )
-            stdscr.addstr(2, 0, 'Press "g" for game' )
-
-        c = stdscr.getch()
-
-
-        if c == ord('t'):
-            stdscr.clear()
-            state = "testing"
-            stdscr.addstr("test")
-
-        if c == ord('d'):
-            state = "dividing"
-            stdscr.clear()
-            stdscr.addstr(1, 0, '10 divided by {} is {}'.format(1, 10/1))
-            stdscr.addstr(7, 0, '10 divided by {} is {}'.format(1, 10/1))
-            
-        if c == ord('g'):
-            state = "gaming"
-
-        if state == "gameover" and c == ord('r'):
-            state = "gaming"
-            game = Game(6, 6)
-
-        if c == curses.KEY_RIGHT:
-            selected_column += 1
-            selected_column = min(selected_column, game.width-1)
-
-        if c == curses.KEY_LEFT:
-            selected_column -= 1
-            selected_column = max(selected_column, 0)
-
-        if c == curses.KEY_DOWN and state == "gaming":
-            game.drop(players_list[turn%len(players_list)], selected_column)
-            turn += 1
-
-        if state == "gaming":
-            stdscr.clear()
-            stdscr.addstr(0, selected_column, 'v')
-            stdscr.addstr(1, 0, '_'*game.width)
-            stdscr.addstr(8, 0, '~'*game.width)
-
-            for k in game.grid:
-                stdscr.addstr(game.height-k[0]+1, k[1], game.grid[k])
-
-        if game.over():
-            state = "gameover"
-            stdscr.addstr(10, 0, 'GAME OVER ....')
-            stdscr.addstr(11, 0, 'Press:')
-            stdscr.addstr(12, 0, '       r) restart')
-            stdscr.addstr(13, 0, '       q) quit')
-            # stdscr.addstr(10, 0, ' _________    _______    _      _    _____   ')
-            # stdscr.addstr(11, 0, '|  _______|  |  ___  |  | \    / |  |  ___|  ')
-            # stdscr.addstr(12, 0, '| |    ___   | |___| |  |  \  /  |  | |___   ')
-            # stdscr.addstr(13, 0, '| |   |_  |  |  ___  |  |   \/   |  |  ___|  ')
-            # stdscr.addstr(14, 0, '| |_____| |  | |   | |  | |\  /| |  | |___   ')
-            # stdscr.addstr(15, 0, '|_________|  |_|   |_|  |_| \/ |_|  |_____|  ')
-
-            # stdscr.addstr(17, 0, ' ________   __        __   _____    _____    ')
-            # stdscr.addstr(18, 0, '|  ____  |  \ \      / /  |  ___|  |  _  |   ')
-            # stdscr.addstr(19, 0, '| |    | |   \ \    / /   | |___   | |_| |   ')
-            # stdscr.addstr(20, 0, '| |    | |    \ \  / /    |  ___|  |    _|   ')
-            # stdscr.addstr(21, 0, '| |____| |     \ \/ /     | |___   | |\ \    ')
-            # stdscr.addstr(22, 0, '|________|      \__/      |_____|  |_| \_\   ')
-            stdscr.refresh()
-
-        if c == ord('q'):
-            break
-        
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
-
+    game = CliGame(stdscr)
+    game.main()
 
 if __name__ == "__main__":
     curses.wrapper(main_ui)
